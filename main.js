71c1eb9d174b23c41bfbaeecc2dc66e10dd7781f
TentativeTarget <m18602021532c@163.com>
2026-09-09 12:00:16 +0800

    Add notes backend and Electron integration

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
 const { app, BrowserWindow } = require('electron/main')
+const { spawn } = require('node:child_process')
+const path = require('node:path')
+
+let pythonServer
+
+const startPythonServer = () => {
+  const pythonCommand = process.platform === 'win32' ? 'python' : 'python3'
+  pythonServer = spawn(pythonCommand, [path.join(__dirname, 'server.py'), '8765'], {
+    stdio: ['ignore', 'pipe', 'pipe']
+  })
+
+  pythonServer.stderr.on('data', data => {
+    console.error(`[Python] ${data}`)
+  })
+}
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
 
   win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
+  startPythonServer()
   createWindow()
 
   app.on('activate', () => {
@@ -20,6 +39,7 @@ app.whenReady().then(() => {
 })
 
 app.on('window-all-closed', () => {
+  pythonServer?.kill()
   if (process.platform !== 'darwin') {
     app.quit()
   }

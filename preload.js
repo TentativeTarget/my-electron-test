e7e7faab912c73b925833374be7b8c50b9012b04
TentativeTarget <m18602021532c@163.com>
2026-09-10 02:31:38 +0800

    Refine collaborative notes interface

--- preload.js
+++ preload.js
@@ -2,6 +2,7 @@ const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('collabNoteApi', {
   baseUrl: process.env.COLLABNOTE_API_URL || 'http://127.0.0.1:8765',
+  openExternal: url => ipcRenderer.invoke('collabnote:open-external', url),
   credentials: {
     save: (username, password) => ipcRenderer.invoke('collabnote:credentials:save', username, password),
     load: () => ipcRenderer.invoke('collabnote:credentials:load'),
